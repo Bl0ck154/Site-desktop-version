41a3d39330d920679a1cdbe23bf686d7137c8bcf
Bl0ck <zail@dublin.com>
2018-11-30 18:56:54 +0200

    getCountries

---
+++ Site-desktop-version/Country.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Site_desktop_version
+{
+	class Country
+	{
+		public int id { get; set; }
+		public string countryName { get; set; }
+	}
+}
--- Site-desktop-version/MainWindow.xaml.cs
+++ Site-desktop-version/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,18 +23,58 @@ namespace Site_desktop_version
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		private string responseJson;
+
 		public MainWindow()
 		{
 			InitializeComponent();
+			LoadComboboxCountriesAsync();
 		}
 
 		private void comboCountry_SelectionChanged(object sender, SelectionChangedEventArgs e)
 		{
+			if ((sender as ComboBox).SelectedIndex == 0)
+				return;
+
 			comboCity.Visibility = Visibility.Visible;
+		}
+		private void LoadComboboxCountriesAsync()
+		{
+			WebRequest request = WebRequest.Create("http://travel.itstep.fun/api/travelApi.php");
+			request.Method = "POST"; // для отправки используется метод Post
+									 // данные для отправки
+			string data = "token=ps_rpo_1&param=getCountries";
+			// преобразуем данные в массив байтов
+			byte[] byteArray = System.Text.Encoding.UTF8.GetBytes(data);
+			// устанавливаем тип содержимого - параметр ContentType
+			request.ContentType = "application/x-www-form-urlencoded";
+			// Устанавливаем заголовок Content-Length запроса - свойство ContentLength
+			request.ContentLength = byteArray.Length;
+			
+			using (Stream dataStream = request.GetRequestStream())
+			{
+				dataStream.Write(byteArray, 0, byteArray.Length);
+			}
+
+			WebResponse response = request.GetResponse();
+			using (Stream stream = response.GetResponseStream())
+			{
+				using (StreamReader reader = new StreamReader(stream))
+				{
+					responseJson = reader.ReadToEnd();
+				}
+			}
+			response.Close();
+			comboCountry.Items.Clear();
+			comboCountry.ItemsSource
+				= JsonConvert.DeserializeObject<List<Country>>(responseJson).Select(c => c.countryName).ToList<string>();
 		}
 
 		private void comboCity_SelectionChanged(object sender, SelectionChangedEventArgs e)
 		{
+			if ((sender as ComboBox).SelectedIndex == 0)
+				return;
+
 			datagridHotels.Visibility = Visibility.Visible;
 		}
 	}
